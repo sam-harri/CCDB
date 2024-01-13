0bf47d4f90a80397cf07329119e0f6171b77d4ee
Dave Moxey <d.moxey@imperial.ac.uk>
2024-01-13 09:11:43 +0000

    Remove uncompiled C++ files and add CI script to detect

--- utilities/Extras/VtkStripsToPolys.cpp
+++ utilities/Extras/VtkStripsToPolys.cpp
@@ -54,7 +54,7 @@ int main(int argc, char *argv[])
     vtkIdType npts;
 #if VTK_MAJOR_VERSION >= 9 ||                                                  \
     (VTK_MAJOR_VERSION >= 8 && VTK_MINOR_VERSION >= 90)
-    const vtkIdType *pts = 0;
+    const vtkIdType *pts = nullptr;
 #else
     vtkIdType *pts = 0;
 #endif
